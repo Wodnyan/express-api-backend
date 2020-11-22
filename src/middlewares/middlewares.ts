ce38593c3fefdf630dda13e8b4465061790ff304
Wodnyan <52217740+Wodnyan@users.noreply.github.com>
2020-11-22 21:21:23 +0200

    Fixed Typo

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -22,6 +22,6 @@ export function errorHandler(
     message: error.message,
     code: statusCode,
     stack: process.env.NODE_ENV === "production" ? undefined : error.stack,
-    erros: error.errors || undefined,
+    errors: error.errors || undefined,
   });
 }
